2f7a005e4b91eb8272f7e5a8dc5cb1fdaa409ade
Vikas Umbadwar <vikasumbadwar@gmail.com>
2023-12-27 14:07:00 +0530

    adding files

---
+++ src/Component/signup.js
@@ -0,0 +1,51 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function SignUp () {
+ 
+    return (
+      <form>
+        <h3>Sign Up</h3>
+        <div className="mb-3">
+          <label>First name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First name"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Last name</label>
+          <input type="text" className="form-control" placeholder="Last name" />
+        </div>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Already registered{' '}
+          <button type="button" className="btn-link">
+            <a href="/sign-in">sign in?</a>
+          </button>
+        </p>
+      </form>
+    );
+  
+}
+export default SignUp;
